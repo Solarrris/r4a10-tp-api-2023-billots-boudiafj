4d205c105c59d0ce9cf35bcb4c1bc12442d00678
Samuel Billot <samuel.billot@outlook.fr>
2023-03-06 20:10:24 +0100

    Displaying the agents

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,59 @@
 "use-strict";
 
-const url = "https://valorant-api.com/v1"
+const url = "https://valorant-api.com/v1";
 
 const getData = async function (url) {
     try {
         const response = await fetch(url);
         const jsResponse = await response.json();
-        return jsResponse.data
+        return jsResponse.data;
     } catch (error) {
         console.error(error);
     }
-}
+};
 
-view.searchButton.addEventListener("click", () => {
-    const agents = getData(url + "/agents/")
-    for (let i = 0; i < agents.length(); i++) {
-        if (agents[i].name.includes())
+view.searchButton.addEventListener("click", async () => {
+    // Showing the loading gif
+    view.waitingIcon.style.display = "block";
+    view.searchingResults.style.display = "none";
+
+    // Removing all children in the results div
+    while (view.searchingResults.firstChild) {
+        view.searchingResults.removeChild(view.searchingResults.firstChild);
     }
-    getData(url + "/agents/" + view.searchInput.value);
-})
+
+    // Getting the agents
+    const agents = await getData(url + "/agents/");
+
+    // Putting the agents that have been searched in an array
+    searchResults = [];
+    for (let i = 0; i < agents.length; i++) {
+        if (
+            agents[i].displayName
+                .toLowerCase()
+                .includes(view.searchInput.value.toLowerCase())
+        ) {
+            searchResults.push(agents[i]);
+        }
+    }
+
+    console.log(searchResults);
+
+    // If agents match the research, show a list of those agents
+    if (searchResults.length !== 0) {
+        for (let i = 0; i < searchResults.length; i++) {
+            const result = document.createElement("p.res");
+            result.textContent = searchResults[i].displayName;
+            view.searchingResults.appendChild(result);
+        }
+    } else {
+        // Else, a "Nothing found" text is displayed
+        const noResult = document.createElement("p.info-vide");
+        noResult.textContent = "(Nothing found)";
+        view.searchingResults.appendChild(noResult);
+    }
+
+    // Putting back the researches and removing the loading gif
+    view.searchingResults.style.display = "block";
+    view.waitingIcon.style.display = "none";
+});
--- js/view.js
+++ js/view.js
@@ -3,8 +3,12 @@
 const view = {
     // Elements
     searchButton: document.getElementById("btn-lancer-recherche"),
-    
-    searchInput: document.querySelector("input[type=text]")
+
+    searchInput: document.querySelector("input[type=text]"),
+
+    waitingIcon: document.getElementById("bloc-gif-attente"),
+
+    searchingResults: document.getElementById("bloc-resultats"),
 
     // Functions
 };

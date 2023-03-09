94727f00e5b25662149df39d658949eb2cca6200
boudiafj <johaina.boudiaf@etu.univ-grenoble-alpes.fr>
2023-03-09 02:28:25 +0100

    working on agent page

--- js/agentController.js
+++ js/agentController.js
@@ -6,7 +6,32 @@ window.onload = async () => {
     document.title = result.displayName;
 
     view.agentName.innerHTML = result.displayName;
-    view.agentIcon.src = result.displayIcon;
+    view.agentIcon.src = result.fullPortrait;
+    view.agentIcon.style.height = "70vh";
+
+    view.agentDescription.innerHTML = result.description;
+    view.roleIcon.src = result.role.displayIcon;
+    view.roleName.innerHTML = result.role.displayName;
+
+    for (let i = 0; i < result.abilities.length; i++) {
+        const ability = result.abilities[i];
+
+        const abilityDiv = document.createElement("div");
+
+        const abilityIcon = document.createElement("img");
+        abilityIcon.style.width = "120px";
+        abilityIcon.style.height = "120px";
+
+        const abilityName = document.createElement("p");
+
+        abilityIcon.src = ability.displayIcon;
+        abilityName.innerHTML = ability.displayName
+
+        abilityDiv.appendChild(abilityIcon);
+        abilityDiv.appendChild(abilityName);
+
+        view.abilities.appendChild(abilityDiv);
+    }
 
     view.waitingGif.style.display = "none";
     view.mainContent.style.display = "block";
--- js/agentView.js
+++ js/agentView.js
@@ -8,7 +8,15 @@ const view = {
 
     waitingGif: document.getElementById("bloc-attente"),
 
-    mainContent: document.getElementById("content")
+    mainContent: document.getElementById("content"),
+
+    agentDescription: document.querySelector("p.desc"),
+
+    roleIcon: document.querySelector("#role img"),
+
+    roleName: document.querySelector("#role p"),
+
+    abilities: document.getElementById("abilities")
 
     // Functions
 };
